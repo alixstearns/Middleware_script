6e7dbfae3322d2d8e92ac67961d7a951da15d8ec
root <root@ip-172-26-8-244.ec2.internal>
2023-09-24 21:39:51 +0000

    agent scripts

---
+++ utrains.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+
+if  [ -f /etc/passwd ]
+ then
+  echo exit
+else
+ echo missing
+
+fi 
+
+
+if [ -d /tmp/serge1 ]
+
+then echo exist
+
+else
+
+mkdir /tmp/serge1
+
+echo new > /tmp/serge1/file
+
+cat /tmp/serge1/file
+
+fi
